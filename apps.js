3fce43274463f8ee5dac0f2f17abf04ac226c8ff
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-11 10:04:49 +0800

    change port to heroku

--- apps.js
+++ apps.js
@@ -1,11 +1,11 @@
 // express set-uo
 var express = require('express')
 var app = express()
-var port = 4000
+var port = process.env.PORT || 4000
 
 // mongoose set-up
 var mongoose = require('mongoose')
-var dbURI = 'mongodb://localhost/test'
+var dbURI = process.env.PROD_MONGODB || 'mongodb://localhost/test'
 mongoose.connect(dbURI)
 
 // check if our connection is okay
