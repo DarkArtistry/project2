5d03ddef12b2e0067c60cde5df41c5d85b5a916e
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-19 15:34:30 +0800

    live version for ajax

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
   $bookmarkbtn.on('click', function () {
     // console.log($(this).val())
     var $bookmarkid = $(this).val()
-
     $.ajax({
       type: 'PUT',
-      url: 'http://localhost:5000/bookmark.json',
+      url: 'https://still-mesa-80925.herokuapp.com/bookmark.json',
+      // url: 'http://localhost:5000/bookmark.json',
       data: {
         articleid: $bookmarkid
       }
